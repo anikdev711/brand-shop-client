0a2394c212fdcc1abc3a099c77e4ca441ec36cd6
anikdev711 <anikdev100@gmail.com>
2023-10-19 19:03:59 +0600

    products added into database by add to cart

--- src/pages/ProductDetails/ProductDetailsCard.jsx
+++ src/pages/ProductDetails/ProductDetailsCard.jsx
@@ -1,18 +1,64 @@
+import Swal from "sweetalert2";
 
 const ProductDetailsCard = ({ productDetails }) => {
-    const { product_image, product_name, product_details } = productDetails;
+    const { product_image, product_name, product_brand, product_type, product_price, product_rating, product_details } = productDetails;
+
+    const handleAddToCartProduct = e => {
+        e.preventDefault();
+        const addToCartProduct = {
+            product_name,
+            product_image,
+            product_brand,
+            product_type,
+            product_price,
+            product_rating,
+            product_details
+        }
+        console.log(addToCartProduct);
+
+        fetch('http://localhost:5000/products', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(addToCartProduct)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.insertedId){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your product has been saved to database',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+
+
+
+    }
+
+
+
     return (
         <div>
             <div className="card lg:card-side bg-base-100 shadow-xl">
                 <figure><img src={product_image} alt="" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{product_name}</h2>
+                    <p>Product Brand: {product_brand}</p>
+                    <p>Product Price: ${product_price}</p>
                     <p>{product_details}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-outline btn-success font-bold">Add to Cart</button>
+                        <button onClick={handleAddToCartProduct} className="btn btn-outline btn-success font-bold">Add to Cart</button>
                     </div>
                 </div>
             </div>
+
+
         </div>
     );
 };
